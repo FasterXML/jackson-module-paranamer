8ac2c6336b3e1f69970ea260bab6e0c26afd5f0e
cpilsworth <cpilsworth@gmail.com>
2014-01-06 21:46:03 +0000

    Change indents to spaces from tabs on TestCreatorWithNamingStrategy

--- src/test/java/com/fasterxml/jackson/module/paranamer/TestCreatorWithNamingStrategy.java
+++ src/test/java/com/fasterxml/jackson/module/paranamer/TestCreatorWithNamingStrategy.java
@@ -6,30 +6,30 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 
 
 public class TestCreatorWithNamingStrategy
-		extends ParanamerTestBase {
+        extends ParanamerTestBase {
 
-	static class CreatorBean
-	{
-		protected String myName;
-		protected int myAge;
+    static class CreatorBean
+    {
+        protected String myName;
+        protected int myAge;
 
-		@JsonCreator
-		public CreatorBean(int myAge, String myName)
-		{
-			this.myName = myName;
-			this.myAge = myAge;
-		}
-	}
+        @JsonCreator
+        public CreatorBean(int myAge, String myName)
+        {
+            this.myName = myName;
+            this.myAge = myAge;
+        }
+    }
 
-	private final ObjectMapper MAPPER = new ObjectMapper()
-			.registerModule(new ParanamerModule())
-			.setPropertyNamingStrategy(PropertyNamingStrategy.PASCAL_CASE_TO_CAMEL_CASE);
+    private final ObjectMapper MAPPER = new ObjectMapper()
+            .registerModule(new ParanamerModule())
+            .setPropertyNamingStrategy(PropertyNamingStrategy.PASCAL_CASE_TO_CAMEL_CASE);
 
-	public void testSimpleConstructor() throws Exception
-	{
-		CreatorBean bean = MAPPER.readValue("{ \"MyAge\" : 42,  \"myName\" : \"NotMyRealName\" }", CreatorBean.class);
-		assertEquals(42, bean.myAge);
-		assertEquals("NotMyRealName", bean.myName);
-	}
+    public void testSimpleConstructor() throws Exception
+    {
+        CreatorBean bean = MAPPER.readValue("{ \"MyAge\" : 42,  \"myName\" : \"NotMyRealName\" }", CreatorBean.class);
+        assertEquals(42, bean.myAge);
+        assertEquals("NotMyRealName", bean.myName);
+    }
 
 }
